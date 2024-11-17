603468b584b0134ff44cb4c7a7f562d50fee3cad
Richard Kooijman <kooijman.richard@gmail.com>
2024-11-17 21:54:50 +0100

    added pull script

---
+++ pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker pull docker.io/library/alpine:latest
+docker pull docker.io/library/mariadb:latest
+docker pull docker.io/fireflyiii/core:latest
+docker pull docker.io/fireflyiii/data-importer:latest
--- restore.sh
+++ restore.sh
@@ -2,7 +2,7 @@
 
 # run in firefly-iii-docker directory
 # set date_string to the backup that's to be restored
-date_string="2024-04-07"
+date_string="2024-10-18"
 
 #tar -zxvf ~/backup/backup-config-${date_string}.tar.gz
 docker run -it --rm -v "firefly-iii-docker_firefly_iii_db:/db" -v "$HOME/backup:/backup" ubuntu tar -zxvf /backup/backup-db-${date_string}.tar.gz -C /db --strip 1
